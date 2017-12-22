23a4438f03e2cbb1244663080b0686e7c34303cd
Chuck Lauer Vose <vosechu@gmail.com>
2017-12-22 09:45:22 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "https://rubygems.org"
+
+gem "binding_of_caller"
+
+group :development, :test do
+  gem "byebug"
+  gem "pry"
+  gem "pry-byebug"
+
+  gem "guard"
+  gem "guard-rspec"
+  gem "rspec"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+RSpec.configure do |config|
+  # These two settings work together to allow you to limit a spec run
+  # to individual examples or groups you care about by tagging them with
+  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  # get run.
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
